aa13d7dadf84e3d8b5a435c814990e808cfcd285
profesor <profesor@profesor-VirtualBox>
2020-02-10 09:53:58 +0100

    Puta vida

---
+++ Refactorizaion/src/Circulo.java
@@ -0,0 +1,114 @@
+
+public class Circulo {
+	
+
+		private int x;
+
+		private int y;
+
+		private double radio;
+
+		private int i;
+
+		public Circulo()
+
+		{
+
+		}
+
+		// constructor
+
+		public Circulo(int valorX, int valorY, double valorRadio)
+
+		{
+
+			x = valorX;
+
+			y = valorY;
+
+			establecerRadio(valorRadio);
+
+		}
+
+		public void establecerX(int valorX)
+
+		{
+
+			x = valorX;
+
+		}
+
+		public int obtenerX()
+
+		{
+
+			return x;
+
+		}
+
+		public void establecerY(int valorY)
+
+		{
+
+			y = valorY;
+
+		}
+
+		public int obtenerY()
+
+		{
+
+			return y;
+
+		}
+
+		public void establecerRadio(double valorRadio)
+
+		{
+
+			radio = (valorRadio < 0.0 ? 0.0 : valorRadio);
+
+		}
+
+		public double obtenerRadio()
+
+		{
+	return radio;
+
+		}
+
+		public double obtenerDiametro()
+
+		{
+
+			return 2 * radio;
+
+		}
+
+		public double obtenerCircunferencia()
+
+		{
+
+			return Math.PI * obtenerDiametro();
+
+		}
+
+		public double obtenerArea()
+
+		{
+
+			return Math.PI * radio * radio;
+
+		}
+
+		public String toString()
+
+		{
+
+			return "Centro = [" + x + ", " + y + "]; Radio = " + radio;
+
+		}
+
+	}
+
+
